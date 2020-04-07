ef63c89734d091c90dbf64bbd20550f9baed0f3d
HBalija <balija.hrvoje@gmail.com>
2020-04-07 21:49:56 +0200

    Create base viewset for tags and ingredients - inheritence

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,35 +7,30 @@ from recipe.models import Ingredient, Tag
 from recipe.serializers import IngredientSerializer, TagSerializer
 
 
-class TagViewsSet(viewsets.GenericViewSet, ListModelMixin, CreateModelMixin):
-    """Manage tags in the database"""
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet, ListModelMixin, CreateModelMixin):
+    """Base viewset for user owned recipe attributes"""
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Return filtered queryset by user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new object"""
+        serializer.save(user=self.request.user)
+
+
+class TagViewsSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
+
     queryset = Tag.objects.all()  # for stupid router both this and get_queryset are required
     serializer_class = TagSerializer
 
-    def get_queryset(self):
-        """Return filtered queryset by user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        """Create new tag"""
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewsSet(viewsets.GenericViewSet, ListModelMixin, CreateModelMixin):
+class IngredientViewsSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
 
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     queryset = Ingredient.objects.all()  # for stupid router both this and get_queryset are required
     serializer_class = IngredientSerializer
-
-    def get_queryset(self):
-        """Return filtered queryset by user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create new ingredient"""
-        serializer.save(user=self.request.user)
