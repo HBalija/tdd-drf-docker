d392691b7d01b8dc82cbac0d2781c57b4f605f90
HBalija <balija.hrvoje@gmail.com>
2020-04-09 10:43:22 +0200

    Add image field to recipe model

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from django.db import models
 from django.conf import settings
 
@@ -18,14 +21,23 @@ class Ingredient(models.Model):
         return self.name
 
 
+def get_recipe_instance_file_path(instance, filename):
+    """ Generate file path for new recipe image """
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
+
+
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-    ingredients = models.ManyToManyField('Ingredient', related_name='recipes')
-    tags = models.ManyToManyField('Tag', related_name="recipes")
+    ingredients = models.ManyToManyField('Ingredient', related_name='recipes', blank=True)
+    tags = models.ManyToManyField('Tag', related_name="recipes", blank=True)
+    image = models.ImageField(blank=True, upload_to=get_recipe_instance_file_path)
 
     def __str__(self):
         return self.title
--- app/recipe/tests/test_models.py
+++ app/recipe/tests/test_models.py
@@ -1,7 +1,9 @@
+from utils.help_test_utils import create_user
+from unittest.mock import patch
+
 from django.test import TestCase
 
-from recipe.models import Ingredient, Recipe, Tag
-from utils.help_test_utils import create_user
+from recipe.models import Ingredient, get_recipe_instance_file_path, Recipe, Tag
 
 
 class TagTest(TestCase):
@@ -24,3 +26,16 @@ class RecipeTest(TestCase):
         recipe = Recipe.objects.create(
             user=create_user(), title='Baked steak', time_minutes=5, price=5.00)
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """ Test that image is saved in correct location """
+
+        uuid = 'test-uuid'
+        # mock return value
+        mock_uuid.return_value = uuid
+
+        file_path = get_recipe_instance_file_path(None, 'myimage.jpg')
+        expected_path = f'uploads/recipe/{uuid}.jpg'
+
+        self.assertEqual(file_path, expected_path)
